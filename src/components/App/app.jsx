f328b3c8fd2a8306486d505a36ab707d60a7be77
kouk02 <kouk20020703@gmail.com>
2024-02-01 13:25:29 +0200

    Fix seatchbar 2

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Section from '../Section/Section';
-import Searchbar from '../Searchbar/Searchbar.jsx';
+import Searchbar from '../Searchbar/searchbar.jsx';
 import Gallery from '../Gallery/Gallery';
 
 
